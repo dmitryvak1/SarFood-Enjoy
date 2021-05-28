448823397df38499ff1d7913bf0310e297e3cbc4
Medovi <62383546+Medovi@users.noreply.github.com>
2021-05-28 19:29:51 +0400

    dfgxgdfhf

--- SarFood/src/main/java/com/example/foodfamily/service/UserService.java
+++ SarFood/src/main/java/com/example/foodfamily/service/UserService.java
@@ -47,7 +47,7 @@ public class UserService implements UserDetailsService {
         if (userFromDb != null) {
             return false;
         }
-        user.setActive(true);
+        user.setActive(false);
         user.setRoles(Collections.singleton(Role.CLIENT));
         user.setActivationCode(UUID.randomUUID().toString());
         user.setPassword(passwordEncoder.encode(user.getPassword()));
@@ -77,6 +77,7 @@ public class UserService implements UserDetailsService {
         }
 
         user.setActivationCode(null);
+        user.setActive(true);
 
         userRepo.save(user);
 
